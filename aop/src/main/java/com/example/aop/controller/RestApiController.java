e5443ded1f4a58d20974767a8294c65a0448f230
powerhorang2 <tjrdbqls3@naver.com>
2021-08-17 17:38:08 +0900

    AOP 예제 - 실무 사례 알아보기 (2) / 메소드 실행시간을 출력하는 메소드를 Custom Annotation으로 Pointcut 잡는 방법 추가

--- aop/src/main/java/com/example/aop/controller/RestApiController.java
+++ aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,5 +1,6 @@
 package com.example.aop.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
 
 @RestController
@@ -22,5 +24,13 @@ public class RestApiController {
 	@PostMapping("/post")
 	public User post(@RequestBody User user) {
 		return user;
+	}
+	
+	@Timer
+	@DeleteMapping("/delete")
+	public void delete() throws InterruptedException {
+		
+		// db logic
+		Thread.sleep(1000 * 2);
 	}
 }
